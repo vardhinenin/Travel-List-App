2a16d965e389bafb103bdf16d3dd6c0094529567
Naveen Rao Vardhineni <nvardhi@iu.edu>
2025-06-26 01:45:59 -0400

    Travel-items-list-App

---
+++ src/Logo.js
@@ -0,0 +1,3 @@
+export function Logo() {
+  return <h1>🌴Far away 💼</h1>;
+}
---
+++ src/Stats.js
@@ -0,0 +1,23 @@
+export function Stats({ items }) {
+  if (!items.length) {
+    return (
+      <footer className="stats">
+        <em>Start adding some items to your packing list 🚀</em>
+      </footer>
+    );
+  }
+
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numPacked / numItems) * 100);
+  return (
+    <footer className="stats">
+      <em>
+        {percentage === 100
+          ? "You got everything! Ready to go ✈️"
+          : ` 👜You have ${numItems} items on your list and you already packed
+        ${numPacked} (${Number(percentage)} %) of your list.`}
+      </em>
+    </footer>
+  );
+}
